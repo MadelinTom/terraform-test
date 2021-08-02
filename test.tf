edffcf1937dcf3e8ca9f0c7b742e02a9a4799529
MadelinTom <tom.madelin@gmail.com>
2021-08-02 14:51:26 +0100

    update

--- test.tf
+++ test.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "test1" {
   provisioner "local-exec" {
-    command = "ibmcloud login"
+    command = "ibmcloud login -a https://cloud.ibm.com"
     interpreter = ["/bin/bash", "-c"]
   }
 }
