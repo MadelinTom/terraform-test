e57c556143327a560106f5de502ba059a6cdada9
MadelinTom <tom.madelin@gmail.com>
2021-08-03 15:14:08 +0100

    update

--- test.tf
+++ test.tf
@@ -1,6 +1,8 @@
 resource "null_resource" "test1" {
   provisioner "local-exec" {
-    command = "ibmcloud oc cluster ls"
+    command     = "ibmcloud login --apikey ${var.ibmcloud_api_key} -r eu-gb -a https://cloud.ibm.com && ibmcloud ob logging config list --cluster ${var.roks_id}"
     interpreter = ["/bin/bash", "-c"]
   }
 }
+
+# ibmcloud ob logging config create --cluster ${var.roks_id} --instance ${var.logdna_id}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ibmcloud_api_key" {
+  type = string
+}
+variable "roks_id" {
+  type = string
+}
+variable "logdna_id" {
+  type = string
+  
+  default = "1234"
+}
